64232b71bef35f43bf69af4bb90848b5d336bc99
Nattsu39 <nattsu39@outlook.com>
2026-05-07 20:13:47 +0800

    feat(seerapi): 添加属性克制关系表

--- packages/solaris/solaris/analyze/analyzers/element_type.py
+++ packages/solaris/solaris/analyze/analyzers/element_type.py
@@ -1,20 +1,25 @@
 from typing import TYPE_CHECKING
 
 from seerapi_models.common import ResourceRef
-from seerapi_models.element_type import ElementType, TypeCombination
+from seerapi_models.element_type import (
+    ElementType,
+    ElementTypeRelation,
+    TypeCombination,
+)
 from solaris.analyze.base import BaseDataSourceAnalyzer, DataImportConfig
 from solaris.analyze.typing_ import AnalyzeResult
 from solaris.utils import split_string_arg
 
 if TYPE_CHECKING:
     from solaris.parse.parsers.skilltype import SkillType
+    from solaris.parse.parsers.types_relation import TypesRelationConfig
 
 
 class ElementTypeAnalyzer(BaseDataSourceAnalyzer):
     @classmethod
     def get_data_import_config(cls) -> DataImportConfig:
         return DataImportConfig(
-            unity_paths=('skillType.json',),
+            unity_paths=('skillType.json', 'typesRelation.json'),
         )
 
     @classmethod
@@ -27,6 +32,29 @@ class ElementTypeAnalyzer(BaseDataSourceAnalyzer):
             item['id']: item for item in data['root']['item']
         }
 
+        name_to_id: dict[str, int] = {
+            item['en'][0]: item['id']
+            for item in element_type_data.values()
+            if item['is_dou'] == 0
+        }
+
+        relation_data: TypesRelationConfig = self._get_data(
+            'unity', 'typesRelation.json'
+        )
+        relations_by_name: dict[str, list[ElementTypeRelation]] = {}
+        for rel in relation_data['root']['relation']:
+            source_name = rel['type']
+            relations_by_name[source_name] = [
+                ElementTypeRelation(
+                    target_type=ResourceRef.from_model(
+                        ElementType, id=name_to_id[opp['type']]
+                    ),
+                    multiple=opp['multiple'],
+                )
+                for opp in rel['opponent']
+                if opp['type'] in name_to_id
+            ]
+
         element_type_map: dict[int, ElementType] = {}
         combination_map: dict[int, TypeCombination] = {}
         for id_, item in element_type_data.items():
@@ -35,6 +63,7 @@ class ElementTypeAnalyzer(BaseDataSourceAnalyzer):
                     id=item['id'],
                     name=item['cn'],
                     name_en=item['en'][0],
+                    relations=relations_by_name[item['en'][0]],
                 )
                 element_type_map[id_] = element_type
 
